52c7f7eb2c3d76e88ac956c42f992ff86e17e3eb
Florian Chabert <65234487+FloDevelops@users.noreply.github.com>
2024-09-03 18:31:33 +0200

    feat: add account and property ids

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,38 @@
-import "./style.css";
+import './style.css';
 
-import { GA4AdminClient, GA4PropertyType } from "./apis/ga4/admin";
-import { displayOauthSignIn, saveCredentials } from "./google";
+import { GA4AdminClient, GA4PropertyType } from './apis/ga4/admin';
+import { displayOauthSignIn, saveCredentials } from './google';
 
 // import gaAmdinIcon from '/icons/ga_admin.svg'
 
-const app = document.querySelector<HTMLDivElement>("#app");
+const app = document.querySelector<HTMLDivElement>('#app');
 
 function searchTable(pattern: string) {
-  let table = app!.querySelector("table")!;
-  let rows = Array.from(table.querySelectorAll("tr"));
+  let table = app!.querySelector('table')!;
+  let rows = Array.from(table.querySelectorAll('tr'));
   rows.shift();
   rows.forEach((row) => {
-    let rowText = Array.from(row.querySelectorAll("td"))
+    let rowText = Array.from(row.querySelectorAll('td'))
       .map((td) => td.textContent)
-      .join(" ");
-    row.style.display = rowText.match(new RegExp(pattern, "i")) ? "" : "none";
+      .join(' ');
+    row.style.display = rowText.match(new RegExp(pattern, 'i')) ? '' : 'none';
   });
 }
 
-let searchField = document.createElement("input");
-searchField.type = "text";
-searchField.placeholder = "Search";
-searchField.addEventListener("input", () => searchTable(searchField.value));
+let searchField = document.createElement('input');
+searchField.type = 'text';
+searchField.placeholder = 'Search';
+searchField.addEventListener('input', () => searchTable(searchField.value));
 app!.insertBefore(searchField, app!.firstChild);
 
 function initEmptyTable() {
-  let table = document.createElement("table");
+  let table = document.createElement('table');
   table.innerHTML = `
     <tr>
+      <th>Account id</th>
       <th>Account name</th>
       <th>Account region</th>
+      <th>Property id</th>
       <th>Property name</th>
       <th>Property currency</th>
       <th>Property timezone</th>
@@ -39,8 +41,10 @@ function initEmptyTable() {
     </tr>
   `;
   for (let i = 0; i < 50; i++) {
-    let row = document.createElement("tr");
+    let row = document.createElement('tr');
     row.innerHTML = `
+      <td></td>
+      <td></td>
       <td></td>
       <td></td>
       <td></td>
@@ -56,9 +60,9 @@ function initEmptyTable() {
 
 app!.appendChild(initEmptyTable());
 
-if (window.location.hash!.includes("googleapis.com")) saveCredentials();
+if (window.location.hash!.includes('googleapis.com')) saveCredentials();
 
-const googleCredentials = localStorage.getItem("google_credentials");
+const googleCredentials = localStorage.getItem('google_credentials');
 if (!googleCredentials) displayOauthSignIn(app!);
 
 declare global {
@@ -68,11 +72,13 @@ declare global {
 }
 
 function displayProperties(accounts: any[] = window.ga4Client) {
-  let table = document.createElement("table");
+  let table = document.createElement('table');
   table.innerHTML = `
     <tr>
+      <th>Account id</th>
       <th>Account name</th>
       <th>Account region</th>
+      <th>Property id</th>
       <th>Property name</th>
       <th>Property currency</th>
       <th>Property timezone</th>
@@ -83,22 +89,22 @@ function displayProperties(accounts: any[] = window.ga4Client) {
   for (let account of accounts) {
     if (!account.properties || account.properties.length === 0) continue;
     for (let property of account.properties) {
-      let property_type_icon = document.createElement("img");
+      let property_type_icon = document.createElement('img');
       switch (property.propertyType) {
-        case "PROPERTY_TYPE_ROLLUP":
-          property_type_icon.src = "/icons/ga_property_rollup.svg";
+        case 'PROPERTY_TYPE_ROLLUP':
+          property_type_icon.src = '/icons/ga_property_rollup.svg';
           break;
-        case "PROPERTY_TYPE_SUBPROPERTY":
-          property_type_icon.src = "/icons/ga_property_sub.svg";
+        case 'PROPERTY_TYPE_SUBPROPERTY':
+          property_type_icon.src = '/icons/ga_property_sub.svg';
           break;
         default:
           break;
       }
 
-      let service_level_icon = document.createElement("img");
+      let service_level_icon = document.createElement('img');
       switch (property.serviceLevel) {
-        case "GOOGLE_ANALYTICS_360":
-          service_level_icon.src = "/icons/ga_property_360.svg";
+        case 'GOOGLE_ANALYTICS_360':
+          service_level_icon.src = '/icons/ga_property_360.svg';
           break;
         default:
           break;
@@ -106,14 +112,16 @@ function displayProperties(accounts: any[] = window.ga4Client) {
 
       if (property.sessionsYesterday) {
         property.sessionsYesterday = Number(
-          property.sessionsYesterday.replace(/,/g, "")
+          property.sessionsYesterday.replace(/,/g, '')
         ).toLocaleString();
       }
 
-      let row = document.createElement("tr");
+      let row = document.createElement('tr');
       row.innerHTML = `
+        <td>${account.id}</td>
         <td>${account.displayName}</td>
         <td>${account.regionCode}</td>
+        <td>${property.id}</td>
         <td>${property.displayName} </td>
         <td>${property.currencyCode}</td>
         <td>${property.timeZone}</td>
@@ -139,19 +147,19 @@ function displayProperties(accounts: any[] = window.ga4Client) {
             <img src="/icons/ga_admin.svg" alt="Admin"/>
           </a>
         </td>
-        <td>${property.sessionsYesterday || "N/A"}</td>
+        <td>${property.sessionsYesterday || 'N/A'}</td>
       `;
       row.children[2].appendChild(property_type_icon);
       row.children[2].appendChild(service_level_icon);
       table.appendChild(row);
     }
   }
-  app!.replaceChild(table, app!.querySelector("table")!);
+  app!.replaceChild(table, app!.querySelector('table')!);
 }
 
 async function fetchAccounts() {
   const ga4AdminClient = new GA4AdminClient(
-    JSON.parse(localStorage.getItem("google_credentials")!).access_token
+    JSON.parse(localStorage.getItem('google_credentials')!).access_token
   );
   const accounts = await ga4AdminClient.listAccounts();
 
@@ -162,7 +170,7 @@ async function fetchAccounts() {
     window.ga4Client = ga4AdminClient;
 
     if (ga4AdminClient.accounts.length > 200) {
-      console.warn("More than 200 accounts, not requesting sessions");
+      console.warn('More than 200 accounts, not requesting sessions');
       displayProperties(window.ga4Client);
       return;
     }
