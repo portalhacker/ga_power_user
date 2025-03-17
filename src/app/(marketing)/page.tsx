4fae99044d6f4831ba8088f83f08088119357fbe
Florian Chabert <65234487+FloDevelops@users.noreply.github.com>
2025-03-17 03:00:34 +0000

    feat: add sign-out

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,13 +1,16 @@
 import Link from 'next/link';
 
 import SignIn from '@/src/components/features/auth/sign-in';
+import SignOut from '@/src/components/features/auth/sign-out';
+import { auth } from '@/src/lib/auth/auth';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
       <h1>Home</h1>
       <nav className="flex flex-col gap-2">
-        <SignIn />
+        {session?.user ? <SignOut /> : <SignIn />}
         <Link href="/account-summaries">Accounts</Link>
       </nav>
     </div>
