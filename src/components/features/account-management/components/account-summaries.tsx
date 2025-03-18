1acda5edb6497ac46e82547e9e207867522df7fc
Florian Chabert <65234487+FloDevelops@users.noreply.github.com>
2025-03-18 03:41:01 +0000

    fix: refactor hook as class for server components

--- src/components/features/account-management/components/account-summaries.tsx
+++ src/components/features/account-management/components/account-summaries.tsx
@@ -2,10 +2,12 @@ import { unstable_cache } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 import { auth, signIn } from '@/lib/auth/auth';
+import { GoogleAnalyticsClient } from '@/lib/google/analytics/google-analytics-client';
 import { prisma } from '@/src/lib/db/prisma';
 import { sortArrayByProperty } from '@/src/lib/utils';
-import useGoogleAnalyticsClient from '../hooks/useGoogleAnalyticsClient';
 import AccountSummariesClient from './account-summaries.client';
+
+export const dynamic = 'force-dynamic';
 
 export default async function AccountSummaries() {
   const session = await auth();
@@ -28,43 +30,41 @@ export default async function AccountSummaries() {
     },
   });
 
-  if (!tokens) {
-    return <p>No Google Analytics credentials.</p>;
+  if (!tokens || !tokens.refresh_token || !tokens.access_token) {
+    return <p>No valid Google Analytics credentials.</p>;
   }
 
-  if (!tokens.refresh_token || !tokens.access_token) {
-    return <p>Invalid Google Analytics credentials.</p>;
-  }
-
-  const client = useGoogleAnalyticsClient({
+  // Initialize the GoogleAnalyticsClient class
+  const client = new GoogleAnalyticsClient({
     refresh_token: tokens.refresh_token,
     access_token: tokens.access_token,
   });
 
-  const getAccountSummaries = unstable_cache(async () => {
+  // Fetch account summaries using `unstable_cache`
+  const accountSummaries = await unstable_cache(async () => {
     return await client.listAccountSummaries();
-  }, ['account-summaries']);
-  const [accountSummaries] = await getAccountSummaries();
+  }, ['account-summaries'])();
 
   if (!accountSummaries) {
-    return <p>Issue fetching...</p>;
-  } else if (accountSummaries.length === 0) {
-    return <p>No Google Analytics accounts found.</p>;
-  } else if (accountSummaries.length > 0) {
-    const sortedAccountSummaries = sortArrayByProperty(
-      accountSummaries,
-      'displayName'
-    );
-    const allKeys = sortedAccountSummaries.map(
-      (accountSummary) => accountSummary.account?.split('/')[1]
-    ) as string[];
-    return (
-      <AccountSummariesClient
-        accountSummaries={sortedAccountSummaries}
-        allKeys={allKeys}
-      />
-    );
-  } else {
-    return <p>Unknown error.</p>;
+    return <p>Issue fetching account summaries.</p>;
   }
+
+  if (accountSummaries.length === 0) {
+    return <p>No Google Analytics accounts found.</p>;
+  }
+
+  const sortedAccountSummaries = sortArrayByProperty(
+    accountSummaries,
+    'displayName'
+  );
+  const allKeys = sortedAccountSummaries.map(
+    (accountSummary) => accountSummary.account?.split('/')[1]
+  ) as string[];
+
+  return (
+    <AccountSummariesClient
+      accountSummaries={sortedAccountSummaries}
+      allKeys={allKeys}
+    />
+  );
 }
