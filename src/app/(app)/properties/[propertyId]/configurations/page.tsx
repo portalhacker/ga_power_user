2249f3db70fa79258989b9a19b3b0e312bbd8e5e
Florian Chabert <65234487+FloDevelops@users.noreply.github.com>
2025-04-03 00:32:36 +0200

    wip: pull all property configurations

---
+++ src/app/(app)/properties/[propertyId]/configurations/page.tsx
@@ -0,0 +1,54 @@
+import SignIn from '@/components/features/auth/sign-in';
+import { auth } from '@/lib/auth/auth';
+import { GoogleAnalyticsClient } from '@/lib/google/analytics/google-analytics-client';
+import { prisma } from '@/src/lib/db/prisma';
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ propertyId: number }>;
+}) {
+  const { propertyId } = await params;
+
+  const session = await auth();
+  if (
+    !session ||
+    session === null ||
+    !session.user ||
+    session?.error === 'RefreshTokenError'
+  ) {
+    return <SignIn />;
+  }
+
+  const tokens = await prisma.account.findFirst({
+    where: {
+      userId: session.user.id,
+      provider: 'google',
+    },
+    select: {
+      refresh_token: true,
+      access_token: true,
+    },
+  });
+
+  if (!tokens || !tokens.refresh_token || !tokens.access_token) {
+    return <p>No valid Google Analytics credentials.</p>;
+  }
+
+  // Initialize the GoogleAnalyticsClient class
+  const client = new GoogleAnalyticsClient({
+    refresh_token: tokens.refresh_token,
+    access_token: tokens.access_token,
+  });
+
+  const propertyDetails = await client.getProperty(propertyId);
+  const allPropertySettings = await client.getAllPropertySettings(propertyId);
+
+  return (
+    <>
+      <div>My Property: {propertyId}</div>
+      {/* <pre>{JSON.stringify(propertyDetails, null, 2)}</pre> */}
+      <pre>{JSON.stringify(allPropertySettings, null, 2)}</pre>
+    </>
+  );
+}
--- src/lib/google/analytics/google-analytics-client.ts
+++ src/lib/google/analytics/google-analytics-client.ts
@@ -21,4 +21,71 @@ export class GoogleAnalyticsClient {
       throw new Error('Failed to fetch account summaries');
     }
   }
+
+  async getProperty(propertyId: number) {
+    try {
+      const [property] = await this.client.getProperty({
+        name: `properties/${propertyId}`,
+      });
+      return property || null;
+    } catch (error) {
+      console.error(`Failed to fetch property with ID ${propertyId}`, error);
+      throw new Error(`Failed to fetch property with ID ${propertyId}`);
+    }
+  }
+
+  async getAllPropertySettings(propertyId: number) {
+    let results: any = {};
+    results['property'] = await this.getProperty(propertyId);
+    if (!results['property']) {
+      console.error(`Property with ID ${propertyId} not found`);
+      throw new Error(`Property with ID ${propertyId} not found`);
+    }
+    const settings = {
+      accessBindings: this.client.listAccessBindings,
+      adSenseLinks: this.client.listAdSenseLinks,
+      audiences: this.client.listAudiences,
+      bigQueryLinks: this.client.listBigQueryLinks,
+      calculatedMetrics: this.client.listCalculatedMetrics,
+      channelGroups: this.client.listChannelGroups,
+      customDimensions: this.client.listCustomDimensions,
+      customMetrics: this.client.listCustomMetrics,
+      dataRetention: this.client.getDataRetentionSettings,
+      dataStreams: this.client.listDataStreams,
+      displayVideo360AdvertiserLinks:
+        this.client.listDisplayVideo360AdvertiserLinks,
+      expandedDataSets: this.client.listExpandedDataSets,
+      firebaseLinks: this.client.listFirebaseLinks,
+      googleAdsLinks: this.client.listGoogleAdsLinks,
+      keyEvents: this.client.listKeyEvents,
+      // reportingDataAnnotations: this.reportingDataAnnotations,
+      rollupPropertySourceLinks: this.client.listRollupPropertySourceLinks,
+      searchAds360Links: this.client.listSearchAds360Links,
+      subpropertyEventFilters: this.client.listSubpropertyEventFilters,
+    };
+    let promises: any = {};
+    for (const [key, method] of Object.entries(settings)) {
+      promises[key] = method.call(this.client, {
+        parent: `properties/${propertyId}`,
+      });
+    }
+    try {
+      const resultsArray = await Promise.allSettled(Object.values(promises));
+      for (let i = 0; i < resultsArray.length; i++) {
+        const key = Object.keys(promises)[i];
+        const result = resultsArray[i];
+        if (result.status === 'fulfilled') {
+          results['property'][key] = result.value[0] || [];
+        }
+        if (result.status === 'rejected') {
+          console.error(`Failed to fetch ${key}`, result.reason);
+          results['property'][key] = null;
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching property settings', error);
+      throw new Error('Error fetching property settings');
+    }
+    return results;
+  }
 }
